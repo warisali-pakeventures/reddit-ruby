13192e05884f35512c2b0239c0e1d15bcaaad7d0
waris.ali <waris.ali@pakeventures.com>
2019-09-20 14:43:43 +0500

    Added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :votes, as: :votable
+
+  validates :title, presence: true
 end
--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -1,4 +1,8 @@
 class Subreddit < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :posts
+
+  validates :name, uniqueness: true
+  validates :name, :description, presence: true
+  validates :description, length: {maximum: 300}
 end
