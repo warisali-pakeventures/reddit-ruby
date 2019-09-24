89f5a55b0f1894ab43de4f6ed5531f666a829cef
waris.ali <waris.ali@pakeventures.com>
2019-09-24 10:41:12 +0500

    Implemented Posts view and show

--- app/views/subreddits/_subreddit.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! subreddit, :id, :name, :description, :created_at, :updated_at
-json.url subreddit_url(subreddit, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :posts
-  resources :subreddits
+
+  resources :subreddits do
+    resources :posts
+  end
 
   root 'users#dashboard'
 
@@ -12,7 +14,5 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   get '/dashboard', to: 'users#dashboard'
 
-  # get '/subreddits/new', to: 'subreddits#new'
-  # post '/subreddits', to: 'subreddits#create'
   get '/r/:subreddit', to: 'subreddits#show_by_name', as: :subreddit_name
 end
